939f698b31afc6f9a530cfe7efe302600a02ae3f
Alan <1356256086@qq.com>
2019-08-04 14:38:54 +0800

    Alan

--- src/main/java/cn/gmsj/evaluationsystem/expertdesign/web/req/ExamineReq.java
+++
@@ -1,8 +0,0 @@
-package cn.gmsj.evaluationsystem.expertdesign.web.req;
-
-import lombok.Data;
-
-@Data
-public class ExamineReq {
-    private  String projectName;
-}
--- src/main/java/cn/gmsj/evaluationsystem/expertdesign/web/req/InviteReq.java
+++
@@ -1,9 +0,0 @@
-package cn.gmsj.evaluationsystem.expertdesign.web.req;
-
-import lombok.Data;
-
-@Data
-public class InviteReq {
-
-    private Long id;
-}
