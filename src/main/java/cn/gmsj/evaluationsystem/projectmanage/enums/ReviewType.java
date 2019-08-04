1bc26fc72b4998c7504babdbe6bce3325b4de086
Alan <1356256086@qq.com>
2019-08-04 17:03:28 +0800

    Alan

---
+++ src/main/java/cn/gmsj/evaluationsystem/projectmanage/enums/ReviewType.java
@@ -0,0 +1,29 @@
+package cn.gmsj.evaluationsystem.projectmanage.enums;
+
+/**
+ * @author 13562
+ */
+
+public enum ReviewType {
+    /**
+     * 初设评审
+     */
+    PRIMARY_DESIGN("初设评审"),
+    /**
+     * 安设评审
+     */
+    SAFE_DESIGN("");
+    private String name;
+
+    ReviewType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
